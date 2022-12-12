96a85855f65baec9f48f03a9e9738c627efa78ea
Evelyn Ortiz <eveortiz10@yahoo.com>
2022-12-12 09:04:55 -0600

    Update 4. SQLpizza_sales.sql

--- Pizza Challenge/4. SQLpizza_sales.sql
+++ Pizza Challenge/4. SQLpizza_sales.sql
@@ -35,35 +35,3 @@ on P.pizza_id = OD.pizza_id
 join [3pizza_types] as PT
 on PT.pizza_type_id = TRES.pizza_type_id
 
----VERIFICAR
-select 
-TRES.order_details_id, 
-TRES.order_id, 
-TRES.pizza_id,
-TRES.quantity,
-TRES.date, 
-TRES.time,
-TRES.size, 
-TRES.price,
-name, category, ingredients
-
-from
-(
-select  
-order_details_id, O.order_id, OD.pizza_id,OD.quantity,
-date, time,
-size, price, pizza_type_id 
-from 
-[1order_details] as OD
-join [2orders] as O
-on O.order_id = OD.order_id
-join [4pizzas] as P
-on P.pizza_id = OD.pizza_id
-) as TRES
-
-join [3pizza_types] as PT
-on PT.pizza_type_id = TRES.pizza_type_id
-
----------------------------------------
-select DISTINCT  order_id
-from [dbo].[2orders]
